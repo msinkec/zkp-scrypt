f2e039cb4c4d085dde850d90b830e0b82ae13a44
msinkec <mihael@sinkec.xyz>
2022-03-18 15:26:43 +0100

    Prover code draft.

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
+from bitcoinx import PrivateKey, sha256, SigHash
 from scryptlib import (
-        compile_contract, build_contract_class, build_type_classes
+        compile_contract, build_contract_class, build_type_classes,
+        create_dummy_input_context, get_preimage_from_input_context,
+        SigHashPreimage
         )
 
 
 if __name__ == '__main__':
+    key_priv = PrivateKey.from_arbitrary_bytes(b'test123')
+    key_pub = key_priv.public_key
 
     contract = './P2GenericECDSAPubKey.scrypt' 
 
@@ -14,7 +19,26 @@ if __name__ == '__main__':
     #with open('./out/checksig_desc.json', 'r') as f:
     #    desc = json.load(f)
     
-
     type_classes = build_type_classes(desc)
 
     P2PK = build_contract_class(desc)
+    p2pk = P2PK(key_pub)
+
+    context = create_dummy_input_context()
+    context.utxo.script_pubkey = p2pk.locking_script
+    sighash_flag = SigHash(SigHash.ALL | SigHash.FORKID)
+    preimage = get_preimage_from_input_context(context, sighash_flag)
+
+    ### Derive proof:
+    r = PrivateKey.from_arbitrary_bytes(b'123test321')
+
+    A = r.public_key
+    G = PrivateKey.from_int(1).public_key
+    st = G.to_bytes(compressed=False) + key_pub.to_bytes(compressed=False)
+
+    e = sha256(preimage + st + A.to_bytes(compressed=False))
+    e = PrivateKey(e)
+    z = key_priv.multiply(e._secret).add(r._secret)
+
+    assert p2pk.unlock(e.to_int(), z.to_int(), SigHashPreimage(preimage)).verify(context)
+
