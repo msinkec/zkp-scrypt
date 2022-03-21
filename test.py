8ba9a4a44df4ff8ba15c430c4bf73b35a850e3ff
msinkec <mihael@sinkec.xyz>
2022-03-21 11:15:42 +0100

    Off-chain verifying test code + point negation fix.

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
-from bitcoinx import PrivateKey, sha256, SigHash
+import json
+
+from bitcoinx import PublicKey, PrivateKey, sha256, SigHash
 from scryptlib import (
         compile_contract, build_contract_class, build_type_classes,
         create_dummy_input_context, get_preimage_from_input_context,
         SigHashPreimage
         )
+
+
+p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
 
 
 if __name__ == '__main__':
@@ -12,17 +17,19 @@ if __name__ == '__main__':
 
     contract = './P2GenericECDSAPubKey.scrypt' 
 
-    compiler_result = compile_contract(contract, debug=False)
-    desc = compiler_result.to_desc()
+    #compiler_result = compile_contract(contract, debug=False)
+    #desc = compiler_result.to_desc()
 
     # Load desc instead:
-    #with open('./out/checksig_desc.json', 'r') as f:
-    #    desc = json.load(f)
+    with open('./out/P2GenericECDSAPubKey_desc.json', 'r') as f:
+        desc = json.load(f)
     
     type_classes = build_type_classes(desc)
+    Point = type_classes['Point']
 
     P2PK = build_contract_class(desc)
-    p2pk = P2PK(key_pub)
+    x, y = key_pub.to_point()
+    p2pk = P2PK(Point({'x': x, 'y': y}))
 
     context = create_dummy_input_context()
     context.utxo.script_pubkey = p2pk.locking_script
@@ -35,10 +42,24 @@ if __name__ == '__main__':
     A = r.public_key
     G = PrivateKey.from_int(1).public_key
     st = G.to_bytes(compressed=False) + key_pub.to_bytes(compressed=False)
-
-    e = sha256(preimage + st + A.to_bytes(compressed=False))
-    e = PrivateKey(e)
+    e = int.from_bytes(sha256(preimage + st + A.to_bytes(compressed=False)), 
+            byteorder='little') % 2**128
+    e = PrivateKey.from_int(e)
     z = key_priv.multiply(e._secret).add(r._secret)
 
+    # Verify proof off-chain:
+    zG = z.public_key
+    ePK = key_pub.multiply(e._secret)
+    ePKx, ePKy = ePK.to_point()
+    ePK_neg = PublicKey.from_point(ePKx, -ePKy % p)
+    A = PublicKey.combine_keys([zG, ePK_neg])
+
+    st = G.to_bytes(compressed=False) + key_pub.to_bytes(compressed=False)
+    bA = A.to_bytes(compressed=False)
+    _e = int.from_bytes(sha256(preimage + st + bA), byteorder='little') % 2**128
+    assert(_e == e.to_int())
+
+
+    # Verify proof on-chain:
     assert p2pk.unlock(e.to_int(), z.to_int(), SigHashPreimage(preimage)).verify(context)
 
